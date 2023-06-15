9f06d4ac3c1736a8f5aae16f9a1f9c69b1c3e3ac
renaudlandry123 <115116385+renaudlandry123@users.noreply.github.com>
2023-06-15 17:07:30 -0400

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default {
       '@nuxtjs/strapi',
     ],
     strapi: {
-      url: process.env.STRAPI_URL || 'https://strapii-vrrush.onrender.com/',
+      url: process.env.STRAPI_URL || 'https://strapii-vrrush.onrender.com',
       prefix: '/api',
       version: 'v4',
       cookie: {},
